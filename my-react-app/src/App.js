2d3252a3d05dec336ef57ab8ef4dcfb7417a0d24
Hrishabh <rishabh6398@gmail.com>
2023-09-18 05:19:21 +0530

    intial setup plus login page

---
+++ my-react-app/src/App.js
@@ -0,0 +1,12 @@
+import LoginPage from './LoginPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/LoginPage.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import './LoginPage.css'; // Import your custom CSS file for styling
+
+const LoginPage = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // You can add your authentication logic here
+        // For this example, we'll simply log the form data
+        console.log('Form data submitted:', formData);
+    };
+
+    return (
+        <div className="login-container">
+            <div className="login-box">
+            <h1>Code Collab</h1>
+                <h3>Login</h3>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="username">Username:</label>
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            value={formData.username}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password:</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default LoginPage;
